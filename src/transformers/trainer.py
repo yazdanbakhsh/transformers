d1bbb4e1e7d0a25ce41d47fc1155cbf22e6f7a5f
Amir Yazdanbakhsh <ayazdan@google.com>
2022-02-14 17:26:45 -0800

    debug grad

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -1439,7 +1439,7 @@ class Trainer:
                         else:
                             # Revert to normal clipping otherwise, handling Apex or full precision
                             for p in model.parameters():
-                              if p.grad:
+                              if torch.all(p.grad):
                                 print(p.grad.view(-1))
                             nn.utils.clip_grad_norm_(
                                 amp.master_params(self.optimizer) if self.use_apex else model.parameters(),
